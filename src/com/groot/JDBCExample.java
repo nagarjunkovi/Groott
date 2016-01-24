be5b556a12355f8aef5789554ddd4877f8d75943
nagarjunkovi <nagarjun.kovi@gmail.com>
2016-01-24 21:15:29 +0530

    Db credentials changed

--- src/com/groot/JDBCExample.java
+++ src/com/groot/JDBCExample.java
@@ -23,7 +23,7 @@ public class JDBCExample {
 
 			try {
 				connection = DriverManager
-				.getConnection("jdbc:mysql://localhost:3303/test","root", "intel@01");
+				.getConnection("jdbc:mysql://localhost:3306/test","root", "root");
 
 			} catch (SQLException e) {
 				System.out.println("Connection Failed! Check output console");
